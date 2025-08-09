4096eaaa36fcf4c9112d126f2d1e1c73ce09dda9
DevMatheusSilva <matheusmello225@gmail.com>
2025-08-09 19:18:49 -0300

    feat: added env variables to environment mapper

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,7 +4,17 @@ dotenv.config();
 
 const environment = {
 	server: {
-		port: process.env.PORT || 3000
+		host: process.env.HOST || 'localhost',
+		port: process.env.PORT || 3000,
+	},
+	database: {
+		postgres: {
+			host: process.env.POSTGRES_HOST || "postgres",
+			port: process.env.POSTGRES_PORT || 5432,
+			username: process.env.POSTGRES_USERNAME || "root",
+			password: process.env.POSTGRES_PASSWORD || "abc12345",
+			database: process.env.POSTGRES_DB || "cozyshelf_db"
+		}
 	}
 };
 
