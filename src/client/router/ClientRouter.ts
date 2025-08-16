1186e8c2b0cb4cfc68011ca22390e383b25cec54
DevMatheusSilva <matheusmello225@gmail.com>
2025-08-16 20:30:24 -0300

    feat: add route for client registration with rendering logic

--- src/client/router/ClientRouter.ts
+++ src/client/router/ClientRouter.ts
@@ -1,8 +1,12 @@
 import {Request, Response, Router} from "express";
 import ClientController from "../controller/ClientController";
 import {ClientControllerFactory} from "../../generic/factories/ClientControllerFactory";
+import ConfigDynamicPaths from "../../generic/middlewares/ConfigDynamicPaths";
+import ClientRegistrationRenderType from "../views/ClientRegistrationRenderType";
+import path from "path";
 
 const clientRouter = Router();
+clientRouter.use(ConfigDynamicPaths.configViewsPath(path.join(__dirname, "../views")));
 
 const clientController: ClientController = new ClientControllerFactory().make();
 
@@ -12,6 +16,15 @@ clientRouter.post("/", async (req: Request, res: Response) => {
 
 clientRouter.get("/", async (req: Request, res: Response) => {
 	await clientController.getAll(req, res);
+});
+
+clientRouter.get("/register", (req: Request, res: Response) => {
+	const renderObject: ClientRegistrationRenderType = {
+		title: "Cadastre-se",
+		message: "Seja muito bem vindo ao cadastro de clientes"
+	};
+
+	res.render("clientRegistration", renderObject);
 });
 
 clientRouter.get("/:id", async (req: Request, res: Response) => {
