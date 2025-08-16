afb98e625480f54fca7e99a339db66809cd361b1
DevMatheusSilva <matheusmello225@gmail.com>
2025-08-15 22:14:59 -0300

    remove: removed unused component

--- src/middlewares/configRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { Express } from "express";
-import defaultRouter from "../routes/default.router";
-
-export default function configRoutes(server: Express) {
-	server.use("/", defaultRouter);
-}
