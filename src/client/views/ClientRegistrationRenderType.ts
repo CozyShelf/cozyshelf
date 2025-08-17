7ca6ed16c7a414e4fd6ac93cb3d0c5ab9a71e0db
DevMatheusSilva <matheusmello225@gmail.com>
2025-08-16 22:23:40 -0300

    refactor: remove unused render type interfaces

--- src/client/views/ClientRegistrationRenderType.ts
+++
@@ -1,5 +0,0 @@
-import IRenderType from "../../generic/views/layouts/IRenderType";
-
-export default interface ClientRegistrationRenderType extends IRenderType {
-	message: string;
-}
--- src/generic/views/layouts/IRenderType.ts
+++
@@ -1,4 +0,0 @@
-export default interface IRenderType {
-	title: string;
-	layout?: string;
-};
