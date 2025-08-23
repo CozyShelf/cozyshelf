8553fd3844d3d6d0e4b258cc40dbca3eab7bb47e
nolascolunardi <nolascolunardi@gmail.com>
2025-08-23 01:09:26 -0300

    feat: implement order routes and views

--- src/generic/routes/defaultRouter.ts
+++ src/generic/routes/defaultRouter.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import clientRouter from "../../client/router/ClientRouter";
 import addressRouter from "../../address/router/AddressRouter";
 import cardRouter from "../../card/router/CardRouter";
+import orderRouter from "../../order/router/OrderRouter";
 
 const defaultRouter = Router();
 
 defaultRouter.use("/clients", clientRouter);
 defaultRouter.use("/addresses", addressRouter);
 defaultRouter.use("/cards", cardRouter);
+defaultRouter.use("/orders", orderRouter);
 
 export default defaultRouter;
---
+++ src/order/router/OrderRouter.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+import ConfigDynamicPaths from "../../generic/helpers/ConfigDynamicPaths";
+import path from "path";
+
+const orderRouter = Router();
+orderRouter.use(
+    ConfigDynamicPaths.configViewsPath(path.join(__dirname, "../views"))
+);
+
+orderRouter.get("/", (req: Request, res: Response) => {
+    res.render("ordersTable", {
+		title: "Meus Pedidos",
+		layout: "detailsLayout",
+		currentUrl: "orders",
+	});
+});
+
+
+orderRouter.get("/:id", (req: Request, res: Response) => {
+    res.render("orderDetails", {
+        title: "Detalhes do Pedido",
+        layout: "detailsLayout",
+        currentUrl: "orders",
+    });
+});
+
+export default orderRouter;
