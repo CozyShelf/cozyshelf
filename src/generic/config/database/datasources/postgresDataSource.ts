91f125879e611ae6ecba12a495587dd5f525177d
DevMatheusSilva <matheusmello225@gmail.com>
2025-08-30 20:36:22 -0300

    fix: corrected default value of logging property

--- src/generic/config/database/datasources/postgresDataSource.ts
+++ src/generic/config/database/datasources/postgresDataSource.ts
@@ -11,7 +11,7 @@ const postgresDataSource = new DataSource({
 	entities: ["src/**/model/*{.js,.ts}"],
 	synchronize: true,
 	dropSchema: true,  // ← Temporariamente true para limpar
-	logging: true,
+	logging: false,
 });
 
 export default postgresDataSource;
