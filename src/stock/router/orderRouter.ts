b00bcf3e518d3d9048c38ecb1638fba49685d111
Ana Laura Lunardi <72282008+nolascolunardi@users.noreply.github.com>
2025-09-05 20:31:19 -0300

    feat: separate admin and client views (#11)
    * feat: add admin dashboard and related views with layout adjustments
    
    * feat: implement admin client details view and update related routes and components

--- src/stock/router/orderRouter.ts
+++ src/stock/router/orderRouter.ts
@@ -16,12 +16,10 @@ stockRouter.get("/", async (req: Request, res: Response) => {
 	res.render("stockTable", {
 		title: "Estoque",
 		currentHeaderTab: "profile",
-		layout: "detailsLayout",
+		layout: "defaultLayoutAdmin",
 		currentUrl: "stock",
-		isAdmin: true,
 		books: books
 	});
 });
-
 
 export default stockRouter;
