b745c397c8c31b5040d4ea6993c8df41924d0bc0
DevMatheusSilva <matheusmello225@gmail.com>
2025-09-07 00:04:34 -0300

    refactor: simplify GenericModel by removing private fields and getters/setters

--- src/generic/model/GenericModel.ts
+++ src/generic/model/GenericModel.ts
@@ -4,24 +4,10 @@ import Entity from "../domain/DomainEntity";
 export default abstract class GenericModel {
 	@PrimaryGeneratedColumn("uuid")
 	@Generated("uuid")
-	_id!: string;
+	id!: string;
 
 	@CreateDateColumn()
-	_createdAt!: Date;
-
-	constructor() {}
-
-	get id(): string {
-		return this._id;
-	}
-
-	get createdAt(): Date {
-		return this._createdAt;
-	}
-
-	set createdAt(value: Date) {
-		this._createdAt = value;
-	}
+	createdAt!: Date;
 
 	abstract toEntity(): Entity;
 }
