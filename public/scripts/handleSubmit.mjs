083ddc32a5a70d32aef86c8857688f4d35eb5710
DevMatheusSilva <matheusmello225@gmail.com>
2025-09-14 14:23:56 -0300

    feat: adicionar ID ao modal SweetAlert para testes

--- public/scripts/handleSubmit.mjs
+++ public/scripts/handleSubmit.mjs
@@ -2,7 +2,7 @@ const forms = document.querySelectorAll("form");
 const modalAddress = document.getElementById("address-modal-container");
 const modalCard = document.getElementById("modal-container");
 
-forms.forEach(form => {
+forms.forEach((form) => {
 	form.addEventListener("submit", (event) => {
 		event.preventDefault();
 		if (form.checkValidity()) {
@@ -13,9 +13,18 @@ forms.forEach(form => {
 				confirmButtonText: "Ok",
 				confirmButtonColor: "#863C13",
 				iconColor: "#D6AE7C",
+				customClass: {
+					container: "success-modal-container",
+				},
+				didOpen: () => {
+					const modal = document.querySelector(".swal2-container");
+					if (modal) {
+						modal.setAttribute("id", "success-modal");
+					}
+				},
 			});
 			form.reset();
-			
+
 			if (modalAddress) {
 				modalAddress.classList.add("hidden");
 				form.classList.add("form-modal-hidden");
