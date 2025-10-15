dc3d635befe6eeb4cd8e92e679ebf6674b7d4aed
DevMatheusSilva <matheusmello225@gmail.com>
2025-10-15 19:48:59 -0300

    feat: implement cart management tests

--- cypress/e2e/cart/addItensToCart.spec.cy.ts
+++ cypress/e2e/cart/addItensToCart.spec.cy.ts
@@ -1,10 +1,15 @@
 import AddCartItensPageObject from "../../support/pages/cart/AddCartItensPageObject";
+import CartManagementPageObject from "../../support/pages/cart/CartManagementPageObject";
 
 describe("add items to cart", () => {
 	let addCartItensPageObject: AddCartItensPageObject;
+	let cartManagementPageObject: CartManagementPageObject;
 
 	const makeSUT = () => {
 		addCartItensPageObject = new AddCartItensPageObject();
+		cartManagementPageObject = new CartManagementPageObject(
+			addCartItensPageObject
+		);
 	};
 
 	beforeEach(() => {
@@ -28,4 +33,16 @@ describe("add items to cart", () => {
 		addCartItensPageObject.addBookToCart();
 		addCartItensPageObject.verifyIfSuccessNotificationAppears();
 	});
-})
+
+	it("should show added books in the cart page", () => {
+		addCartItensPageObject.visitBookDetailsPage();
+		addCartItensPageObject.addBookToCart();
+		addCartItensPageObject.verifyIfSuccessNotificationAppears();
+
+		cartManagementPageObject.visitCartPage();
+
+		cartManagementPageObject.verifyIfBookIsInCart(
+			"da27ae01-0bc9-4509-97bd-2e1eb4fe8dab"
+		);
+	});
+});
